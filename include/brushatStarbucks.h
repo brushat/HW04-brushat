0d56e31cfd0afe95d5e8638712402d5492d25e78
Adam Brush <brushat@gmail.com>
2012-10-27 11:03:15 -0400

    Added some headers and resources
    Added necessary header and resource files from starbucks starter file.
    Also started to create brushatStarbucks.h file

---
+++ include/brushatStarbucks.h
@@ -0,0 +1,30 @@
+/*****
+ * Author   : brushat
+ * Date     : 2012-10-27
+ * Sources  : All code is original
+ * Purpose  : 
+ *            In your class you should declare all of your methods to be virtual: This is how
+ *            polymorphism is accomplished in C++. Do NOT copy the part that says "= 0" that I use
+ *            below. That part means that the function, though declared, will not have any implementation.
+ * Note     : DO NOT CHANGE THIS FILE!!! 
+ */
+
+#pragma
+#include "Starbucks.h"
+
+class brinkmwjStarbucks : public Starbucks {
+public:
+	
+	/* Note: If you detect that two items have the same coordinates, then do not add the new item
+	 *       that has the same coordinates as another item. This is guaranteed to happen, by the way,
+	 *       because some Starbucks locations are listed in the database twice. We will define two locations
+	 *       to be the "same location" if both |x1 - x2| <= 0.00001 and |y1 - y2| < 0.00001
+	 */
+	void build(Entry* c, int n) = 0;
+	
+	/*
+	 * Return a pointer to the entry that is closest to the given coordinates. Your
+	 *  answer may be approximate, but then you will lose points on the "Accuracy" quality measure
+	 */
+	Entry* getNearest(double x, double y) = 0;
+};
